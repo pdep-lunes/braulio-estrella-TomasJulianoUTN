e51fb8d47c062e5755b7007df13c0a3e29c93678
TomasJulianoUTN <tjuliano@frba.utn.edu.ar>
2025-04-28 12:20:16 -0300

    Agrega habilidades y estadisticas de Espinas y Pamela
    No runea correctamente, hay que arreglarlo.

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,3 +4,35 @@ import Text.Show.Functions ()
 
 doble :: Int -> Int
 doble x = x * 2
+
+type Poder = Personaje -> Personaje
+
+data Personaje = UnPersonaje {
+    nombre :: String,
+    poderBasico :: Poder,
+    superPoder :: Poder,
+    superPoderActivo :: Bool,
+    vida :: Double
+}
+
+espinas = UnPersonaje "Espinas" bolaEspinosa granadaDeEspinas 4800
+pamela = UnPersonaje "Pamela" lluviaDeTuercas torretaCurativa 9600
+
+bolaEspinosa :: Poder
+bolaEspinosa contrincante
+  |vida contrincante > 1000 = contrincante {vida = vida contrincante - 1000}
+  |otherwise = contrincante {vida = 0}
+
+granadaDeEspinas :: Poder
+granadaDeEspinas contrincante radioDeExplosion
+  |radioDeExplosion > 3 && vida contrincante < 800 = contrincante {superPoderActivo = False, vida = 0}
+  |radioDeExplosion > 3 = contrincante {nombre = nombre ++ " Espinas estuvo aquí"}
+  |otherwise = bolaEspinosa contrincante
+
+lluviaDeTuercas :: String -> Bool -> Poder
+lluviaDeTuercas tipoDeTuerca esEnemigo personaje
+  |tipoDeTuerca == "sanadoras" && esEnemigo == False = personaje {vida = vida personaje + 800}
+  |tipoDeTuerca == "dañinas" && esEnemigo == True = personaje {vida = vida personaje / 2}
+
+torretaCurativa :: Poder
+torretaCurativa aliado = aliado {superPoderActivo = True, vida = doble (vida aliado)}
